f4f08527c05f094ffa940d851e864787254c9122
ialexanderbrito <xanderzinho26@gmail.com>
2020-11-19 23:42:37 -0300

    ✨ Finalizado Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,194 @@
-import React from 'react';
+/* eslint-disable global-require */
+/* eslint-disable no-underscore-dangle */
+import React, { useRef, useState } from 'react';
+import {
+  Animated,
+  Dimensions,
+  TouchableWithoutFeedback,
+  View,
+  FlatList,
+} from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+import SlidingUpPanel from 'rn-sliding-up-panel';
+import { MaterialIcons } from '@expo/vector-icons';
 
-import { Text } from 'react-native';
+import Add from '../../components/Add';
+import Users from '../../components/Users';
+import Operation from '../../components/Operation';
 
-import { Container } from './styles';
+import data from '../../components/Users/data';
+
+import {
+  Wrapper,
+  Container,
+  TopBar,
+  Welcome,
+  Name,
+  Profile,
+  ProfileNotification,
+  Card,
+  SendMoney,
+  SendContainer,
+  Slider,
+  PanelContainer,
+  PanelHandle,
+  TitlePanel,
+  PanelSpace,
+  PanelItemContainer,
+  PanelItem,
+  Avatar,
+  NamePanel,
+  TransactionPanel,
+  AmountPanel,
+  PanelButton,
+  PanelButtonText,
+} from './styles';
 
 const Home: React.FC = () => {
+  const { width, height } = Dimensions.get('window');
+  const carouselRef = useRef(null);
+
+  const [dragRange, setDragRange] = useState({
+    top: height - 80,
+    bottom: 65,
+  });
+  const _draggedValue = new Animated.Value(180);
+  const ModalRef = useRef(null);
+
+  const Images = [
+    {
+      image: require('../../assets/card2.png'),
+    },
+    {
+      image: require('../../assets/card1.png'),
+    },
+    {
+      image: require('../../assets/card3.png'),
+    },
+    {
+      image: require('../../assets/card4.png'),
+    },
+  ];
+
+  const RenderItem = ({ item }) => {
+    return (
+      <TouchableWithoutFeedback>
+        <Card source={item.image} />
+      </TouchableWithoutFeedback>
+    );
+  };
+
   return (
-    <Container>
-      <Text>Home</Text>
-    </Container>
+    <Wrapper>
+      <Container>
+        <TopBar>
+          <View>
+            <Welcome>Bem-vindo, de volta</Welcome>
+            <Name>Alexander</Name>
+          </View>
+
+          <View>
+            <Profile
+              source={{ uri: 'https://www.github.com/ialexanderbrito.png' }}
+            />
+            <ProfileNotification />
+          </View>
+        </TopBar>
+
+        <View>
+          <Carousel
+            layout="default"
+            ref={carouselRef}
+            data={Images}
+            renderItem={RenderItem}
+            sliderWidth={width}
+            itemWidth={width - 10}
+            swipeThreshold={100}
+            layoutCardOffset={-12}
+            inactiveSlideOpacity={0.4}
+            containerCustomStyle={{ overflow: 'visible', marginVertical: 30 }}
+            contentContainerCustomStyle={{ paddingTop: 10 }}
+          />
+        </View>
+
+        <View>
+          <SendMoney>Enviar dinheiro</SendMoney>
+          <SendContainer>
+            <Add />
+            <Users />
+          </SendContainer>
+          <SendMoney>Operações</SendMoney>
+          <Operation />
+        </View>
+      </Container>
+
+      <Slider>
+        <SlidingUpPanel
+          ref={ModalRef}
+          draggableRange={dragRange}
+          animatedValue={_draggedValue}
+          backdropOpacity={0}
+          snappingPoints={[360]}
+          height={height + 20}
+          friction={0.9}
+        >
+          <PanelContainer>
+            <PanelHandle />
+            <View>
+              <TitlePanel>Transações recentes</TitlePanel>
+            </View>
+
+            <PanelSpace>
+              <FlatList
+                showsVerticalScrollIndicator={false}
+                data={data}
+                keyExtractor={(item) => item.key}
+                renderItem={({ item }) => {
+                  return (
+                    <PanelItemContainer>
+                      <PanelItem>
+                        <View style={{ marginRight: 10 }}>
+                          <Avatar source={{ uri: item.userImage }} />
+                        </View>
+                        <View>
+                          <NamePanel>{item.userName}</NamePanel>
+                          <TransactionPanel>
+                            {item.transactionDate}
+                          </TransactionPanel>
+                        </View>
+                      </PanelItem>
+                      <View
+                        style={{ flexDirection: 'row', alignItems: 'center' }}
+                      >
+                        <AmountPanel>{item.amount}</AmountPanel>
+                        {item.credit ? (
+                          <MaterialIcons
+                            name="arrow-drop-up"
+                            size={22}
+                            color="green"
+                          />
+                        ) : (
+                          <MaterialIcons
+                            name="arrow-drop-down"
+                            size={22}
+                            color="red"
+                          />
+                        )}
+                      </View>
+                    </PanelItemContainer>
+                  );
+                }}
+              />
+            </PanelSpace>
+            <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+              <PanelButton>
+                <PanelButtonText>Ver tudo</PanelButtonText>
+              </PanelButton>
+            </View>
+          </PanelContainer>
+        </SlidingUpPanel>
+      </Slider>
+    </Wrapper>
   );
 };
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,155 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+import Constants from 'expo-constants';
 
+import colors from '../../styles/colors';
+
+const statusBarHeight =
+  Platform.OS === 'android' ? Constants.statusBarHeight : 0;
+
+export const Wrapper = styled.SafeAreaView`
+  flex: 1;
+  background: ${colors.primary};
+  padding-top: ${`${statusBarHeight}px`};
+`;
 export const Container = styled.View`
+  padding-top: 25px;
+  padding-horizontal: 14px;
+`;
 
+export const TopBar = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Welcome = styled.Text`
+  font-size: 14px;
+  color: ${colors.bluedark};
+  font-family: 'OpenSans_700Bold';
+`;
+
+export const Name = styled.Text`
+  font-size: 20px;
+  color: ${colors.gray};
+  font-family: 'OpenSans_700Bold';
+`;
+
+export const Profile = styled.Image`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+`;
+
+export const ProfileNotification = styled.View`
+  height: 12px;
+  width: 12px;
+  background-color: ${colors.green};
+  border-radius: 6px;
+  position: absolute;
+  border-width: 2px;
+  border-color: ${colors.black};
+`;
+
+export const Card = styled.Image`
+  width: 360px;
+  height: 240px;
+  border-radius: 10px;
+`;
+
+export const SendMoney = styled.Text`
+  font-family: 'OpenSans_400Regular';
+  color: ${colors.gray};
+  margin-bottom: 10px;
+`;
+
+export const SendContainer = styled.View`
+  flex-direction: row;
+`;
+
+export const Slider = styled.View`
+  flex: 1;
+`;
+
+export const PanelContainer = styled.View`
+  flex: 1;
+  background-color: ${colors.primary};
+  border-radius: 24px;
+  padding: 14px;
+`;
+
+export const PanelHandle = styled.View`
+  height: 5px;
+  width: 50px;
+  background-color: ${colors.gray};
+  border-radius: 6px;
+  align-self: center;
+  margin-top: 6px;
+`;
+
+export const TitlePanel = styled.Text`
+  font-family: 'OpenSans_400Regular';
+  color: ${colors.gray};
+  margin-vertical: 16px;
+`;
+
+export const PanelSpace = styled.View`
+  height: 500px;
+  padding-bottom: 10px;
+`;
+
+export const PanelItemContainer = styled.View`
+  border-width: 0.4px;
+  border-color: ${colors.gray};
+  padding: 14px;
+  border-radius: 6px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+export const PanelItem = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Avatar = styled.Image`
+  width: 30px;
+  height: 30px;
+  border-color: ${colors.black};
+  border-radius: 40px;
+`;
+
+export const NamePanel = styled.Text`
+  font-family: 'OpenSans_600SemiBold';
+  font-size: 14px;
+  color: ${colors.black};
+`;
+
+export const TransactionPanel = styled.Text`
+  font-family: 'OpenSans_400Regular';
+  font-size: 10px;
+  color: ${colors.gray};
+`;
+
+export const AmountPanel = styled.Text`
+  font-family: 'OpenSans_600SemiBold';
+  font-size: 16px;
+  color: ${colors.black};
+  margin-horizontal: 14px;
+`;
+
+export const PanelButton = styled.TouchableOpacity`
+  padding: 14px;
+  width: 200px;
+  justify-content: center;
+  background-color: ${colors.bluedark};
+  border-radius: 10px;
+`;
+
+export const PanelButtonText = styled.Text`
+  font-family: 'OpenSans_600SemiBold';
+  font-size: 16px;
+  color: ${colors.black};
 `;
